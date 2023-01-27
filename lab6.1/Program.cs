32ab473a827e5461374739c8e6f013acc4bc8f40
glebe <glebe@BELG04>
2023-01-27 16:23:56 +0200

    Добавьте файлы проекта.

---
+++ lab6.1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Лабраторная:5.1 Уровень:средний Лукьянов Глеб 
+
+
+    class Program
+    {
+        static int Euclid(int a, int b)
+        {
+            if (a > b)
+            {
+                a = a % b;
+            }
+            else
+            {
+                b = b % a;
+            }
+            int result = a + b;
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int c;
+            Console.Write("Ведите число а: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ведите число b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            c = Euclid(a, b);
+            Console.WriteLine(c);
+        }
+    }
